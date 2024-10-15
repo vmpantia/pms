875b49d48a5e0be529e53a6c1bd691dc84ed085a
Vincent Pantia <vincent.m.pantia@hotmail.com>
2024-10-16 01:12:17 +0800

    Minor: Create interfaces for BaseRepository and WorkItemRepository

---
+++ PMS/PMS.Shared/Contracts/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace PMS.Shared.Contracts.Repositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        IQueryable<TEntity> Get();
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> expression);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ PMS/PMS.Shared/Contracts/Repositories/IWorkItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using PMS.Shared.Models.Entities;
+using System.Linq.Expressions;
+
+namespace PMS.Shared.Contracts.Repositories
+{
+    public interface IWorkItemRepository : IBaseRepository<WorkItem>
+    {
+        Task<IEnumerable<WorkItem>> GetWorkItemsAsync(
+            Expression<Func<WorkItem, bool>>? expression = null, 
+            CancellationToken cancellationToken = default);
+    }
+}
